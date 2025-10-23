b306b8419d1daf398b56b18329a4112029eb4f44
romeji <lopes.jerome21@gmail.com>
2025-10-23 11:11:14 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,32 @@
+
+
+
+<footer>
+		<div class="container" id="main1">
+			<div class="row">
+
+				<div class="col-md-2 col-lg-2 col-xl-2 ">
+	                <h6><strong><font color="#1984B1">A propos</font></strong></h6><br/>
+	                <p style="font-size: 12px; font-family: Verdana"><a href="contact.php">Contact </a></p>
+	                <p style="font-size: 12px; font-family: Verdana"><a href="#!">Plan du site</a></p>
+	                <p style="font-size: 12px; font-family: Verdana"><a href="mentionslegales.php">Mentions légales</a></p>
+	                
+	            </div>
+
+				<div class="col-md-3 col-lg-3 col-xl-3">
+	                <h6><strong><font color="#1984B1">Contact </font> </strong></h6><br/>
+	                <p style="font-size: 12px; font-family: Verdana">7 Rue joseph cugnot <br/>21800 Sennecey-les-Dijon </p>
+	           	    <p style="font-size: 12px; font-family: Verdana">lopes.jerome21@gmail.com </p>
+	            </div>
+
+	            <div class="col-md-5 col-lg-5 col-xl-5 ">
+	                <h6><strong><font color="#1984B1"> Pourquoi ce site ?</font></strong></h6>
+	                <p style="font-size: 13px; font-family:Helvetica Neue,Arial,Helvetica,sans-serif;">Je suis étudiant à Dijon Formation ainsi que développeur au sein de 
+					l'entreprise SAP2I, Je suis passionné par le développement informatique et les nouvelles technologies en général, 
+					c'est donc en toute logique que j'ai choisi de concevoir un site web. Ce site me sert de portfolio, 
+					mais également de support pour partager mes cours et mes projets.</p>
+					<br/><br/>
+	            </div>
+			</div>
+		</div>
+   	</footer>
